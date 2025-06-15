ecbd58af28e91f3ec0341e212fce1a51517d9ee4
Hafizulshah <hafizulshah2002@gmail.com>
2025-06-15 10:02:47 +0800

    add new due date field in db

--- src/ToDoListManagement.Core/Entities/ToDoItem.cs
+++ src/ToDoListManagement.Core/Entities/ToDoItem.cs
@@ -7,4 +7,5 @@ public class ToDoItem
     public string? Description { get; set; }
     public bool IsCompleted { get; set; } = false;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? DueDate { get; set; }
 }
